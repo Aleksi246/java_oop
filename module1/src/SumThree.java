4e622bb3cf3ffb89d56a4ae151fc4eaa42d3821a
Aleksi <aleklap@metropolia.fi>
2025-08-18 12:13:49 +0300

    commit with task 1,2 and 3

---
+++ module1/src/SumThree.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SumThree {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int first = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int second = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Give the third number:");
+        int third = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("The sum of the numbers is " + (first + second + third));
+
+        System.out.println("The product of the numbers is " + (first * second * third));
+// first version of average rounds down. program is propably using int type as default for numbers.
+        System.out.println("The average of the numbers is " + (first + second + third)/3f);
+//as expected changing the nuber to float fixed the problem.
+    }
+}
---
+++ module1/src/pyramidi.java
@@ -0,0 +1,8 @@
+public class pyramidi {
+    public static void main(String[] args) {
+        System.out.println("    *\n" +
+                "   ***\n" +
+                "  *****\n" +
+                " *******");
+    }
+}
