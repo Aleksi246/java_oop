4d41e94c3a83f391751a99a41eb8dd0eb898989b
Aleksipc <aleklap@metropolia.figit config --global user.emailgit config --global user.email aleklap@metropolia.fi>
2025-09-06 14:48:37 +0300

    4 done

---
+++ module4.0/src/Calculator.java
@@ -0,0 +1,32 @@
+public class Calculator {
+    private int currentValue = 0;
+
+    public void reset() {
+        currentValue = 0;
+    }
+
+    public void add(int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("Negative values not allowed");
+        }
+        currentValue += value;
+    }
+
+    public int getValue() {
+        return currentValue;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        calc.add(5);
+        calc.add(10);
+        System.out.println("Current value: " + calc.getValue()); // Should print 15
+        calc.reset();
+        System.out.println("After reset: " + calc.getValue()); // Should print 0
+        try {
+            calc.add(-3);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+    }
+}
